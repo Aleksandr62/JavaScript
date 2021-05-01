9739440e47c30dd82bcefa915faf7f046f153b7d
Aleksandr62 <74791135+Aleksandr62@users.noreply.github.com>
2021-05-01 15:35:16 +0300

    Lesson 4-HW

--- script.js
+++ script.js
@@ -1,210 +1,83 @@
 'use strict';
-const products = [
-    { id: 1, name: 'Ноутбук', price: 35000, },
-    { id: 2, name: 'Монитор', price: 12000, },
-    { id: 3, name: 'Мышка', price: 1000, },
-    { id: 4, name: 'Клавиатура', price: 2000, },
-];
+/* 1. Дан большой текст, в котором для оформления прямой речи используются одинарные кавычки. 
+Придумать шаблон, который заменяет одинарные кавычки на двойные. */
+const text = `Дан большой текст, в котором 'для оформления прямой речи используются' одинарные кавычки. 
+Придумать шаблон, 'который заменяет одинарные' кавычки на двойные.`;
+console.log(text.replace(/\'/g, `"`));
 
-const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+/* 2. Улучшить шаблон так, чтобы в конструкциях типа aren't 
+одинарная кавычка не заменялась на двойную. */
+const newtext = `Дан большой текст, в котором 'для оформления прямой aren't речи используются' одинарные кавычки. 
+Придумать шаблон, 'который заменяет одинарные' aren't кавычки на двойные.`;
+console.log(newtext.replace(/\s\'/g, ` "`).replace(/\'\s/g, `" `));
 
-/*
-//Задача 1 -------------------------------------------
-// Переделать в ДЗ не использовать fetch а Promise
-let makeGetRequest = (url) => {
-	return new Promise((resolve, reject) => {
-		let xhr = new XMLHttpRequest();
-		xhr.open('GET', url, true);
-		xhr.send();
-    xhr.onreadystatechange = () => {
-        if (xhr.readyState === 4) {
-            if (xhr.status !== 200) {
-                console.log('Error');
-            } else {
-                resolve(xhr.response);
-            }
-        }
-    };
-
-	})
-
-};
+/* 
+3. Создать форму обратной связи с полями: Имя, Телефон, E-mail, текст, кнопка Отправить. 
+При нажатии на кнопку Отправить произвести валидацию полей следующим образом:
+a. Имя содержит только буквы.
+b. Телефон имеет вид +7(000)000-0000.
+c. E-mail имеет вид mymail@mail.ru, или my.mail@mail.ru, или my-mail@mail.ru.
+d. Текст произвольный.
+e. Если одно из полей не прошло валидацию, необходимо выделить это поле красной рамкой 
+и сообщить пользователю об ошибке. 
 */
 
-class ProductItem {
-    constructor(product, img = './image.png') {
-        this.id = product.product_id;
-        this.img = img;
-        this.name = product.product_name;
-        this.price = product.price;
-        // стоит добавить this.quantity - кол-во товаров в магазине и
-        // метод уменьшения (резервирования до продажи) кол-ва при добавлении в корзину 
+class FeedBack {
+    constructor(container = 'div.fb') {
+        document.querySelector(container).insertAdjacentHTML('beforeend', this.render());
+        this._init();
+    }
+    _init() {
+        document.querySelector('input.feedback__button').disabled = true;
+        document.querySelector('form.feedback').addEventListener('focusout', (e) => {
+            let regexp = /.*/;
+            switch (e.target.id) {
+                case 'fb-name':
+                    regexp = /^([a-zа-я]+)$/;
+                    break;
+                case 'fb-phone':
+                    regexp = /^(\+[7]\(\d{3}\)\d{3}\-\d{4})$/;
+                    break;
+                case 'fb-email':
+                    regexp = /^([a-z0-9_\.-]+)@([a-z0-9_\.-]+)\.([a-z\.]{2,6})$/;
+                    break;
+            }
+            if (e.target.id !== 'fb-text' && e.target.type !== 'button')
+                (!this._validate(e.target, regexp)) ?
+                    (e.target.classList.add('warning'), e.target.nextElementSibling.hidden = false, e.target.nextElementSibling.dataset.check = false) :
+                    (e.target.classList.remove('warning'), e.target.nextElementSibling.hidden = true, e.target.nextElementSibling.dataset.check = true);
+            if (document.querySelectorAll('[hidden]').length === 3 && document.querySelectorAll('[data-check]').length === 3)
+                document.querySelector('input.feedback__button').disabled = false;
+        });
+        document.querySelector('#fb-phone').addEventListener('keydown', (e) => {
+            if (e.keyCode === 8 || e.keyCode === 9 || e.keyCode === 46) return;
+            e.preventDefault();
+            if (e.target.value === '') e.target.value = '+7(';
+            if (e.target.value.length === 6) e.target.value += ')';
+            if (e.target.value.length === 10) e.target.value += '-';
+            if (e.target.value.length === 15) return;
+            if (+e.key >= 0 && +e.key < 10) e.target.value += e.key;
+        });
+    }
+    _validate(el, regExp) {
+        return regExp.test(el.value);
     }
     render() {
-        return `<div class="product__item" data-id=${this.id}>
-        <img class="product__image" src="${this.img}" alt="Image">
-        <h3>${this.name}</h3>
-        <p>${this.price} &#8381;</p>
-        <button class="product__btn">Добавить в корзину</button>
-      </div>`;
+        return `<form class="feedback" action="">
+                <label for="fb-name">Имя:</label>
+                <input id="fb-name" type="text" size="40">
+                <span class="spanWarning" hidden>Введите правильные данные</span>
+                <label for="fb-phone">Телефон:</label>
+                <input id="fb-phone" type="text" placeholder="+7(000)000-0000">
+                <span class="spanWarning" hidden>Введите правильные данные</span>
+                <label for="fb-email">E-mail:</label>
+                <input id="fb-email" type="email" placeholder="example@example.com">
+                <span class="spanWarning" hidden>Введите правильные данные</span>  
+                <label for="fb-text">Сообщение:</label>
+                <input id="fb-text" type="text" placeholder="..." size="255"> 
+                <input class="feedback__button" type="button" value="Отправить" onclick="alert('Сообщение отправлено.')">                                               
+                </form>`;
     }
 }
 
-class ProductList {
-    constructor(products, container = '.products') {
-        this.container = document.querySelector(container);
-        this.goods = [];
-        this.goodsLinks = [];
-        this.fetchGoods(`${API}/catalogData.json`)
-            .then((data) => {
-        	this.goods = data;
-	})
-            .then(() => {
-        	this.render();
-	})
-
-    }
-    fetchGoods(url) {
-// Задача 3 -------------------------------------------	
-	return new Promise((resolve, reject) => {
-		let xhr = new XMLHttpRequest();
-		xhr.open('GET', url, true);
-		xhr.responseType = 'json';
-		xhr.send();
-		xhr.onload = () => resolve(xhr.response);
-	})
-
-/* Задача 1 -------------------------------------------	
- 	return makeGetRequest(`${API}/catalogData.json`)
-            .then((data) => {
-                this.goods = JSON.parse(data);
-	}) */
-
-/*        return fetch(`${API}/catalogData.json`)
-            .then((response) => response.json())
-            .catch((error) => {
-                console.log(error);
-            }); */
-    }
-    summaryProducts() {
-        return this.goods.reduce((sum, good) => {
-            return sum + good.price;
-        }, 0);
-    }
-    getProductById(id) {
-        return this.goods.filter((good) => {
-            if (good.id === +id) return good;
-        });
-    }
-    render() {
-	this.goodsLinks = [];
-        this.goods.forEach((good) => {
-            this.goodsLinks.push(new ProductItem(good))
-        });
-        this.goodsLinks.forEach((good) => {
-            this.container.insertAdjacentHTML('beforeEnd', good.render())
-        });
-    }
-}
-
-// класс продукта в корзине
-class OrderItem {
-    constructor(product, img = './image.png') {
-        this.id = product.product_id;
-        this.img = img;
-        this.name = product.product_name;
-        this.quantity = product.quantity;
-        this.price = product.price;
-    }
-    summaryProduct() {
-        return this.quantity * this.price;
-    }
-    render() {
-        return `<div class="bascet__item" data-id=${this.id}>
-        <img class="bascet__image" src="${this.img}" alt="Image">
-	<div>
-        <h3>${this.name}</h3>
-        <p>Кол-во: ${this.quantity}</p>
-        <p>Цена за ед.: ${this.price} &#8381;</p>
-        <p>Сумма: ${this.summaryProduct()} &#8381;</p>
-	</div>
-        <button class="bascet__btn">Удалить из корзины</button>
-      </div>`;
-    }
-}
-
-// класс корзины
-class Bascet {
-    constructor(container = '.bascet') {
-        this.container = document.querySelector(container);
-        this.goods = [];
-        this.goodsLinks = [];
-	this.getBascetProduct(`${API}/getBasket.json`)
-            .then((data) => {
-        	this.goods = data.contents;
-	})
-            .then(() => {
-        	this.render();
-	})
-    }
-// из задачи 2 запрос на сервер список продуктов из getBasket.json
-    getBascetProduct(url) {
-	return new Promise((resolve, reject) => {
-		let xhr = new XMLHttpRequest();
-		xhr.open('GET', url, true);
-		xhr.responseType = 'json';
-		xhr.send();
-		xhr.onload = () => resolve(xhr.response);
-	})
-    }
-    addProductToBascet(url) {
-           if (this.goods.length === 0) this.goods.push(product);
-            else
-                this.existsProductInBascet(product) ? this.increaseQuant(product) : this.goods.push(product);
-        }
-        // увеличение кол-ва (если в корзине есть такой товар)
-    increaseQuant(product) {
-            this.goods.forEach((good) => {
-                if (product.id === good.id) good.quantity++;
-            });
-        }
-        // проверка есть ли в корзине добавляемый продукт
-    existsProductInBascet(product) {
-        return this.goods.some((good) => { return product.id === good.id });
-    }
-    deleteItemFromBascet(id) {
-
-        }
-        // 2. Добавьте для корзины (GoodsList) метод, определяющий суммарную стоимость всех товаров.
-    summaryOrder() {
-        return this.goods.reduce((sum, good) => {
-            return sum + good.price * good.quantity;
-        }, 0);
-    }
-    render() {
-	this.goodsLinks = [];
-        this.goods.forEach((good) => {
-            this.goodsLinks.push(new OrderItem(good))
-        });
-        this.goodsLinks.forEach((good) => {
-            this.container.insertAdjacentHTML('beforeEnd', good.render())
-        });
-    }
-}
-
-const catalog = new ProductList(products);
-const bascet = new Bascet();
-document.querySelector('body').addEventListener('click', (event) => {
-		if(event.target.className === 'btn-cart') {
-			bascet.container.style.display = 'flex';
-			catalog.container.style.display = 'none';
-			bascet.container.innerText = '';
-			bascet.render(); 
-		}
-		if(event.target.className === 'logo') {
-			bascet.container.style.display = 'none';
-			catalog.container.style.display = 'flex';
-			catalog.container.innerText = '';
-			catalog.render(); 
-		}
-});
+new FeedBack();
