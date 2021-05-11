950159c9adf566f926eaadf7442e7d3c59979f0b
Aleksandr62 <74791135+Aleksandr62@users.noreply.github.com>
2021-05-11 23:08:04 +0300

    lesson7
    Заметил что при быстром добавлении и удалении товаров может количество в корзину на сервере записываться не соответствующее корзине на фронте.
    предполагаю, что не успевает then отработать...
    Может как вариант блокировать (условие добавить) функцию, пока не будет результат.
    И поправил в добавлении - кол-во добавляемого товара на фронте обернул в then при условии положительного результата (1) - надеюсь правильно))

---
+++ server/handler.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const cart = require('./cart');
+//const stat = require('./stat.js'); //////////////////
+
+const actions = {
+  add: cart.add,
+  change: cart.change,
+  delete: cart.del,
+};
+
+const handler = (req, res, action, file) => {
+  fs.readFile(file, 'utf-8', (err, data) => {
+    if (err) {
+      res.sendStatus(404, JSON.stringify({result: 0, text: err}));
+    } else {    
+      const newCart = actions[action](JSON.parse(data), req);   
+      fs.writeFile(file, newCart, (err) => {
+        if (err) {
+          res.send('{"result": 0}');
+        } else {
+          //stat(req, 'add', '../server/db/stat.json'); ///////////////////
+          res.send('{"result": 1}');
+        }
+      })
+    }
+  });
+};
+
+module.exports = handler;
